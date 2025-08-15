bbb6e6c93edeab1f5c49e7495f7388fd0477cfe0
arshacodes <133303310+arshacodes@users.noreply.github.com>
2025-08-16 06:51:40 +0800

    Initial commit

---
+++ src/views/errors/NoPageFound.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+    <div class="h-screen flex flex-col items-center justify-center text-red-600">
+        <h1 class="text-2xl font-bold">404</h1>
+        <p class="text-lg">Page Not Found</p>
+    </div>
+</template>
